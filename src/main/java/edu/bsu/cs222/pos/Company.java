e600ecfcee531afcb6c7c4dbd3612569b1c72e3f
Owen Walters <owenwalters.net@gmail.com>
2020-10-28 02:58:46 -0400

    start adding sql support

--- src/main/java/edu/bsu/cs222/pos/Company.java
+++ src/main/java/edu/bsu/cs222/pos/Company.java
@@ -1,12 +1,36 @@
 package edu.bsu.cs222.pos;
 
 import java.math.BigDecimal;
+import java.sql.*;
 import java.util.HashMap;
 
 public class Company {
     private String companyName;
     private HashMap<String,Item> inventoryList = new HashMap<>();
+    private Connection db;
 
+    public Company(String companyName) {
+        this.companyName = companyName;
+        StringBuilder dbName = new StringBuilder();
+        for (int i = 0; i < companyName.length(); i++) {
+            char c = companyName.charAt(i);
+            if (Character.isLetterOrDigit(c)){
+                dbName.append(Character.toLowerCase(c));
+            }
+        }
+        try {
+            db = DriverManager.getConnection("jdbc:derby:"+dbName+";create=true");
+            Statement statement = db.createStatement();
+            DatabaseMetaData dbMd = db.getMetaData();
+            ResultSet rs = dbMd.getTables(null, null, "ITEMS", null);
+            if (!rs.next()) {
+                statement.execute(Item.createTable);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
     public void addItem(String barcodeNumber, Item item){
         inventoryList.put(barcodeNumber,item);
     }
@@ -24,9 +48,6 @@ public class Company {
         updateItem(barcodeNumber,item);
     }
 
-    public Company(String companyName) {
-        this.companyName = companyName;
-    }
 
     public HashMap<String, Item> getAvailableInventoryList() {
         return inventoryList;
--- src/main/java/edu/bsu/cs222/pos/Item.java
+++ src/main/java/edu/bsu/cs222/pos/Item.java
@@ -1,5 +1,8 @@
 package edu.bsu.cs222.pos;
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Item {
     public  BigDecimal getPrice() {
@@ -25,4 +28,12 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public static String createTable = "CREATE TABLE ITEMS ("+
+            "ID varchar(40) NOT NULL,"+
+            "Price decimal(18,2) NOT NULL,"+
+            "Name varchar(32672) NOT NULL,"+
+            "Image varchar(32672),"+
+            "PRIMARY KEY (ID)" +
+            ")";
 }
