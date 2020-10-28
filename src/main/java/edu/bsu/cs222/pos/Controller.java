4b147d05c783f4bd9c8b87f6b4dcf51459a09e55
danchepkwony <dan.k.chepkwony@gmail.com>
2020-10-28 03:21:56 -0400

    Separates Control and View

--- src/main/java/edu/bsu/cs222/pos/Controller.java
+++ src/main/java/edu/bsu/cs222/pos/Controller.java
@@ -1,5 +1,10 @@
 package edu.bsu.cs222.pos;
 
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
 import java.math.BigDecimal;
 
 public class Controller {
@@ -8,7 +13,32 @@ public class Controller {
     }
 
     public static void addItemsToDisplay(){
+        //TODO: loop through items to display
         Item exampleItem = new Item("Example", new BigDecimal("2.0"));
         Display.displayItems(exampleItem);
+    }
+
+    public static void toggleEditOpacity(TextField companyNameInput, ToggleButton editCompanyName){
+        companyNameInput.setEditable(false);
+        companyNameInput.setOnMouseClicked(event -> companyNameInput.setEditable(true));
+        companyNameInput.setOnAction(event -> companyNameInput.setEditable(false));
+        companyNameInput.editableProperty().bindBidirectional(editCompanyName.selectedProperty());
+        editCompanyName.setOnAction(event -> {
+            if (editCompanyName.isSelected()) {
+                companyNameInput.requestFocus();
+            }
+        });
+        companyNameInput.styleProperty().bind(
+                Bindings.when(
+                        companyNameInput.editableProperty())
+                        .then((String) null)
+                        .otherwise("-fx-background-color: transparent;")
+        );
+        companyNameInput.alignmentProperty().bind(
+                Bindings.when(
+                        companyNameInput.editableProperty())
+                        .then(Pos.CENTER_LEFT)
+                        .otherwise(Pos.CENTER)
+        );
     }
 }
--- src/main/java/edu/bsu/cs222/pos/Display.java
+++ src/main/java/edu/bsu/cs222/pos/Display.java
@@ -3,7 +3,6 @@ package edu.bsu.cs222.pos;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.beans.binding.Bindings;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
@@ -15,9 +14,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-
-import java.math.BigDecimal;
 
 public class Display extends Application{
     private final Label titleLabel = new Label("Admin Panel");
@@ -33,10 +32,9 @@ public class Display extends Application{
         private final HBox itemsListHeader = new HBox(
                 itemsListLabel,
                 addItem);
-        private static final TableView<Object> itemList = new TableView<>();
-        private final TableColumn<Object, Object> nameColumn = new TableColumn<>("Item Name");
-        private final TableColumn<Object, Object> priceColumn = new TableColumn<>("Price");
-        private final TableColumn<Object, Object> updateButtonColumn = new TableColumn<>("");
+        private static final TableView<Item> itemList = new TableView<>();
+        private final TableColumn<Item, Item> nameColumn = new TableColumn<>("Item Name");
+        private final TableColumn<Item, Item> priceColumn = new TableColumn<>("Price");
         private final ScrollPane itemListScrollPane = new ScrollPane(itemList);
 
         @Override
@@ -44,7 +42,7 @@ public class Display extends Application{
             primaryStage.setWidth(1000);
             primaryStage.setHeight(600);
             formatDisplay();
-            toggleEditOpacity();
+            Controller.toggleEditOpacity(companyNameInput, editCompanyName);
             primaryStage.setScene(new Scene(createRoot()));
             primaryStage.show();
         }
@@ -59,29 +57,29 @@ public class Display extends Application{
             return root;
         }
 
+        @SuppressWarnings("unchecked")
         private void formatDisplay(){
             titleLabel.setMinWidth(1000);
             companyNameInput.setMinWidth(400);
             itemsListHeader.setMinWidth(900);
-            nameColumn.setMinWidth(300);
-            priceColumn.setMinWidth(300);
-            updateButtonColumn.setMinWidth(300);
+            nameColumn.setMinWidth(450);
+            priceColumn.setMinWidth(450);
             itemList.setMinWidth(900);
             itemListScrollPane.setMinWidth(900);
             titleLabel.setFont(Font.font("Arial", 16));
             companyNameLabel.setFont(Font.font("Arial", 15));
             companyNameInput.setFont(Font.font("Arial", 20));
-            itemsListLabel.setFont(Font.font("Arial", 15));
+            itemsListLabel.setFont(Font.font("Arial", FontWeight.BLACK, FontPosture.REGULAR, 15));
             HBox.setMargin(companyNameLabel, new Insets(10, 20, 10, 140));
             HBox.setMargin(companyNameInput, new Insets(10, 20, 10, 20));
             HBox.setMargin(itemsListLabel, new Insets(15, 20, 10, 50));
-            HBox.setMargin(addItem, new Insets(10, 20, 10, 690));
+            HBox.setMargin(addItem, new Insets(10, 20, 10, 680));
             itemList.setTranslateX(50);
             titleLabel.setAlignment(Pos.CENTER);
             companyField.setAlignment(Pos.CENTER_LEFT);
             nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
             priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-            itemList.getColumns().addAll(nameColumn, priceColumn, updateButtonColumn);
+            itemList.getColumns().addAll(nameColumn, priceColumn);
             itemListScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
             itemListScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
             itemListScrollPane.setVvalue(.5);
@@ -89,34 +87,7 @@ public class Display extends Application{
             itemListScrollPane.setDisable(false);
         }
 
-        private void toggleEditOpacity(){
-            companyNameInput.setEditable(false);
-            companyNameInput.setOnMouseClicked(event -> companyNameInput.setEditable(true));
-            companyNameInput.setOnAction(event -> companyNameInput.setEditable(false));
-            companyNameInput.editableProperty().bindBidirectional(editCompanyName.selectedProperty());
-            editCompanyName.setOnAction(event -> {
-                if (editCompanyName.isSelected()) {
-                    companyNameInput.requestFocus();
-                }
-            });
-            companyNameInput.styleProperty().bind(
-                    Bindings.when(
-                            companyNameInput.editableProperty())
-                            .then((String) null)
-                            .otherwise("-fx-background-color: transparent;")
-            );
-            companyNameInput.alignmentProperty().bind(
-                    Bindings.when(
-                            companyNameInput.editableProperty())
-                            .then(Pos.CENTER_LEFT)
-                            .otherwise(Pos.CENTER)
-            );
-        }
-
         public static void displayItems(Item item){
-            System.out.println(item);
-            Item exampleItem = new Item("Example", new BigDecimal("2.0"));
-            Button update = new Button("Update");
             itemList.getItems().add(item);
             itemList.refresh();
         }
