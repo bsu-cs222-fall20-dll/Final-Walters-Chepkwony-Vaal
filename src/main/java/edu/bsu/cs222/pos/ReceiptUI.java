4d9066738e5452717e682c3ed61709bfa48686bf
wangyouxuan <ywang5@bsu.edu>
2020-11-09 15:40:33 -0500

    working on ReceiptUI

--- src/main/java/edu/bsu/cs222/pos/ReceiptUI.java
+++ src/main/java/edu/bsu/cs222/pos/ReceiptUI.java
@@ -1,6 +1,9 @@
 package edu.bsu.cs222.pos;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -13,17 +16,16 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 
 public class ReceiptUI extends Application{
     private final Label titleLabel = new Label("Company Name");
-    private final Label itemLabel = new Label("Item Name");
-    private final Label priceLabel = new Label("Price");
     private final Label taxLabel = new Label("Tax:");
     private final Label balanceLabel = new Label("Balance:");
     private final Label dateAndTimeLabel = new Label("Date and Time:");
     private final HBox receiptItems = new HBox();
     private final HBox receiptTotal = new HBox();
-
     private static final TableView<Item> receiptItemList = new TableView<>();
     private final TableColumn<Item, Item> receiptNameColumn = new TableColumn<>("Item Name");
     private final TableColumn<Item, Item> receiptPriceColumn = new TableColumn<>("Price");
@@ -48,7 +50,6 @@ public class ReceiptUI extends Application{
                 taxLabel,
                 balanceLabel,
                 dateAndTimeLabel
-
         );
         return root;
     }
@@ -56,15 +57,34 @@ public class ReceiptUI extends Application{
     private void formatDisplay(){
         titleLabel.setMinWidth(400);
         titleLabel.setMinHeight(50);
+        titleLabel.setFont(Font.font("Arial", 25));
+        titleLabel.setTranslateY(5);
+        titleLabel.setAlignment(Pos.CENTER);
         receiptItems.setMinWidth(400);
         receiptItems.setMinHeight(400);
         receiptItems.setMinWidth(400);
         receiptItems.setMinHeight(150);
-        titleLabel.setFont(Font.font("Arial", 25));
-        titleLabel.setAlignment(Pos.CENTER);
+        receiptNameColumn.setMinWidth(180);
+        receiptItemList.setMinWidth(350);
+        receiptItemList.setTranslateX(50);
+        receiptItemList.getColumns().addAll(receiptNameColumn, receiptPriceColumn);
+        taxLabel.setFont(Font.font("Arial", 20));
+        HBox.setMargin(taxLabel, new Insets(10, 5, 10, 175));
+        balanceLabel.setFont(Font.font("Arial", 20));
+        HBox.setMargin(balanceLabel, new Insets(10, 5, 10, 175));
+        dateAndTimeLabel.setFont(Font.font("Arial", 20));
+        HBox.setMargin(dateAndTimeLabel, new Insets(10, 5, 10, 175));
+        receiptItemListScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        receiptItemListScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        receiptItemListScrollPane.setVvalue(.5);
+        receiptItemListScrollPane.setHvalue(.5);
+        receiptItemListScrollPane.setDisable(false);
     }
 
-    public static void displayItem(){
+    public static void displayItem(ArrayList<Item> data){
+        ObservableList<Item> observableData = FXCollections.observableList(data);
+        receiptItemList.setItems(observableData);
+        receiptItemList.refresh();
     }
 
 }
